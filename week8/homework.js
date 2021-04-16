9212814e3f840472b7708fffc8cab84d89468051
YiTing1110 <kikicat901110@gmail.com>
2021-04-16 12:03:25 +0800

    YiTing1110

---
+++ week8/homework.js
@@ -0,0 +1,31 @@
+//01
+function star(t)
+{
+    for(var i=0; i<t; i++)
+    {
+        console.log('*')
+    }
+}
+star(5)
+
+//02
+function number(m, n)
+{
+    for(; m<=n; m++)
+    {
+        console.log(m);
+    }
+}
+number(3, 8)
+
+//03
+function prime(a,b)
+{
+    for(; a<=b; a++)
+    {
+        for(var ; ; i++)
+        {
+        }
+    }
+}
+prime(3,15)
