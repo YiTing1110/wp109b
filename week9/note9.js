bb5b6d29753085a92d06f25e3ff38221dab9376b
YiTing1110 <kikicat901110@gmail.com>
2021-04-25 22:49:46 +0800

    YiTing1110

--- week9/note9.js
+++ week9/note9.js
@@ -1,22 +1,41 @@
 var a = [[1,2,3],[4,5,6]];
-
-for(var i = 0; i<a.length; i++)
+var l = "";
+for(var i = 0; i < a.length; i++)
 {
-    for(var j = 0; i<a[0].length; j++)
+    for(var j = 0; j < a[i].length; j++)
     {
-        console.log("a[%d] = %d", a, a[j]);
+        l += a[i][j] + " ";
+        console.log(a[i][j]);
     }
+    console.log(a[i]);
 }
+console.log(l);
+console.log(a);
+
+console.log();
 
 function add(a, b)
 {
-    var i = a + b;
-    console.log(i);
+    return a + b;
 }
-
+//匿名函數
 var sub = function(a, b)
 {
-    add(a, b);
+    return a + b;
 }
+var plus = sub;
+console.log("add(3, 5) = "+add(3, 5));
+console.log("sub(5 + 7) =", sub(5, 7), "plus(7 + 9) =", plus(7, 9));
 
-sub(10, 5);
+console.log();
+
+var a = 5, b = [0, 2, 4], c = [7, 9, 11];
+function changeValue(a, b, c)
+{
+    a += 1;
+    b[0] += b[0] + 1;
+    c += 1;
+}
+console.log(a, b, c[0]);
+changeValue(a, b, c[0]);
+console.log(a, b, c[0]);
